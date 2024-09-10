cb4d07a42b9f028691282852fd53c00f0c31b2d7
Magpayo, Novesteras, David <davidjimwel@gmail.com>
2024-09-10 22:04:21 +0800

    multiplication

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+ 
+
+public class calculator {
+
+ 
+
+ 
+
+    public static int multiplication(int a, int b) {
+
+ 
+
+        return a * b;
+
+ 
+
+
+
+    }
+
+
+ 
+
+ 
+
+    
