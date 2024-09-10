c63bd196e936a79f73bbe33a5bd2d316e6be0378
Magpayo, Novesteras, David <davidjimwel@gmail.com>
2024-09-10 21:55:13 +0800

    subtraction

---
+++ calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+ 
+
+public class calculator {
+
+ 
+
+ 
+
+    public static int subtraction(int a, int b) {
+
+ 
+
+        return a - b;
+
+ 
+
+    }
+
+
+ 
+
+ 
+
+    
