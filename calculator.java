53b6ad222d2b8695d4a1fe1ccf528d853394ab57
Magpayo, Novesteras, David <davidjimwel@gmail.com>
2024-09-10 22:06:36 +0800

    division

---
+++ calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+ 
+
+public class calculator {
+
+ 
+
+ 
+
+    public static int division(int a, int b) {
+
+ 
+
+        return a / b;
+
+
+ 
+
+
+
+    }
+
+
+ 
+
+ 
+
+    
